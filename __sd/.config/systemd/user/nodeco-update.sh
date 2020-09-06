19bed0a9c357a11592f6791eaf2941136661f54a
k-hizaki <k-hizaki>
2020-09-06 11:19:50 +0000

    fix

--- __sd/.config/systemd/user/nodeco-update.sh
+++ __sd/.config/systemd/user/nodeco-update.sh
@@ -6,11 +6,12 @@ function clean() {
 	exit 1
 }
 
-if [ -d ${HOME}/nodeco-sd ] && [ -f ${HOME}/nodeco-sd/sd-update.yml ]; then
-	ansible-playbook ${HOME}/nodeco-sd/sd-update.yml
-else
+if [ ! -d ${HOME}/nodeco-sd ] || [ ! -f ${HOME}/nodeco-sd/sd-update.yml ]; then
 	rm -rf ${HOME}/nodeco-sd
 	git clone https://github.com/spengejp/nodeco-sd.git ${HOME}/nodeco-sd
+fi
+
+if [ -d ${HOME}/.nodeco-client ] && [ -f ${HOME}/.nodeco-client/.update ]; then
 	ansible-playbook ${HOME}/nodeco-sd/sd-update.yml
 fi
 
